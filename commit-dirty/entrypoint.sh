b6ba5a539907c2cb9d5853e4ca747c381989742b
Bo Jeanes <me@bjeanes.com>
2019-03-22 18:45:40 +1100

    Fix outputting diff to commit

--- commit-dirty/entrypoint.sh
+++ commit-dirty/entrypoint.sh
@@ -5,13 +5,13 @@ set -e
 if ! git diff --quiet ${WORKDIR}; then
 	: ${GIT_COMMITTER_NAME:=$GITHUB_ACTOR}
 	: ${GIT_COMMITTER_EMAIL:="$GITHUB_ACTOR@users.noreply.github.com"}
+	
+	git diff --stat ${WORKDIR}
 
 	git add -A ${WORKDIR}
 	git commit -m "$*"
 else
 	echo Working tree ${WORKDIR:-"."} clean
-
-	git diff --stat ${WORKDIR}
 
 	# https://developer.github.com/actions/creating-github-actions/accessing-the-runtime-environment/#exit-codes-and-statuses
 	exit ${NO_CHANGES_TO_COMMIT_EXIT_CODE:-78}
