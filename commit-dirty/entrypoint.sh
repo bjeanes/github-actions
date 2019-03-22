5ebd9db49f92ba0d629adf2a145989ec405bf763
Bo Jeanes <me@bjeanes.com>
2019-03-22 22:02:29 +1100

    Fix so we can commit brand new files

--- commit-dirty/entrypoint.sh
+++ commit-dirty/entrypoint.sh
@@ -2,16 +2,21 @@
 
 set -e
 
-if ! git diff --quiet ${WORKDIR}; then
+WORKDIR=${WORKDIR:-"."}
+
+# https://stackoverflow.com/a/857696/56690
+git add -N $WORKDIR
+
+if ! git diff --quiet $WORKDIR; then
 	git config user.name "${GIT_COMMITTER_NAME:-"$GITHUB_ACTOR"}"
 	git config user.email "${GIT_COMMITTER_EMAIL:-"$GITHUB_ACTOR@users.noreply.github.com"}"
 
-	git diff --stat ${WORKDIR}
+	git diff --stat $WORKDIR
 
-	git add -A ${WORKDIR}
+	git add -A $WORKDIR
 	git commit -m "$*"
 else
-	echo Working tree ${WORKDIR:-"."} clean
+	echo Working tree $WORKDIR clean
 
 	# https://developer.github.com/actions/creating-github-actions/accessing-the-runtime-environment/#exit-codes-and-statuses
 	exit ${NO_CHANGES_TO_COMMIT_EXIT_CODE:-78}
